d95a68461bd83cff4622b58fd74af8d5612b642e
Kong <kongpazam@gmail.com>
2024-08-26 15:47:41 +0700

    update - collapse

--- src/components/content/Overview.vue
+++ src/components/content/Overview.vue
@@ -4,13 +4,14 @@ import Employee from '@/components/content/vannilaDetail/Employee.vue'
 import Department from '@/components/content/vannilaDetail/Department.vue'
 import HumanResource from '@/components/content/vannilaDetail/HumanResource.vue'
 import Promotion from '@/components/content/vannilaDetail/Promotion.vue'
+import Question from '@/components/content/vannilaDetail/Question.vue'
 </script>
 <template>
   <div class="flex-column-center gap-32 mr-24 ml-24">
-    <HumanResource/>
-    <Employee/>
-    <Department/>
-    <Promotion/>
+    <HumanResource />
+    <Employee />
+    <Department />
+    <Promotion />
+    <Question />
   </div>
 </template>
-
